5113cc67d4d4f506a2aef7b00159d1fbed292947
LouisBertin <bertin.louis7@gmail.com>
2017-04-17 13:56:45 +0200

    add last dist folder

---
+++ dist/header.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>zion.css</title>
+    <link rel="stylesheet" href="assets/css/zion.min.css">
+    <link rel="stylesheet" href="assets/css/demo.min.css">
+    <link rel="stylesheet" href="assets/css/prism.min.css">
+</head>
+<body>
+<div class="container">
+    
+<div class="col-12 txtCenter">
+    <h2 class="fs-32 m-40">Zion.css</h2>
+</div>
+    <nav class="m-20">
+        <div class="grid_sm-1">
+            <div class="col">
+                <a class="black" href="index.php">
+                    <div class="nav-item txtCenter">
+                        Install
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="black" href="grid.php">
+                    <div class="nav-item txtCenter">
+                        Grid
+                    </div>
+                </a>
+            </div>
+        </div>
+    </nav>
---
+++ dist/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<?php include "header.php"; ?>
+
+    <div class="col-12">
+        <h1>The way to start</h1>
+        <hr>
+        <br>
+        <article>
+            <p class="fs-20">
+               1. Download from the repository :
+                <a href="https://github.com/LouisBertin/Zion" target="blank">https://github.com/LouisBertin/Zion</a>
+            </p>
+        </article>
+        <article>
+            <p class="fs-20">
+                2. Put the file into the head section :
+            </p>
+            <br>
+            <pre class="noPadding"><code class="language-markup"><script type="prism-html-markup"><!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+    <link rel="stylesheet" href="assets/css/zion.min.css">
+</head>
+<body>
+  <!-- some stuff here -->
+</body>
+</html></script></code></pre>
+        </article>
+    </div>
+
+</div>
+
+<script src="assets/js/main.min.js"></script>
+
+</body>
+</html>
